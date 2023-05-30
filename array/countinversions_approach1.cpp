b498c6592d4b73b9e2638ab50d93c2e6e80c5801
Divyanshi_Singh_Bora <bora.1@iitj.ac.in>
2023-05-30 16:41:26 +0530

    count_inversions01

---
+++ array/countinversions_approach1.cpp
@@ -0,0 +1,48 @@
+//~ author      : DSB
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+ 
+ 
+#define ll                      long long
+#define pint                    pair<int,int>
+#define plong                   pair<long long,long long>
+#define vlong                   vector <long long>
+#define vint                    vector <int>
+#define endl                    "\n"
+#define pb                      push_back
+#define mp                      make_pair
+#define for0(i,n)               for(int i=0;i<n;i++)
+#define for1(i,n)               for(int i=1;i<=n;i++)
+template <class t>              void printvector(vector<t>& v, ll n) {for0(i,n) {cout<<v[i]<<" ";} cout<<endl;}
+//-----------------------------------------------------------------------------------------------------------
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+    cin>>v[i];
+    int count=0;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(v[i]>v[j])
+            {
+                cout<<v[i]<<" "<<v[j]<<endl;
+                count++;
+            }
+        }
+    }
+    cout<<count<<endl;
+}
+int main()
+{
+// int t;
+// cin>>t;
+// for(int i=0;i<t;i++)
+{
+solve();
+}
+}
