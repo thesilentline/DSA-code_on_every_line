351135b6664d330448b04263ecfd53adad9b90ce
Divyanshi_Singh_Bora <bora.1@iitj.ac.in>
2023-06-18 14:13:11 +0530

    update

---
+++ binarysearchtree/inordersuccesor.cpp
@@ -0,0 +1,86 @@
+//~ author      : DSB
+#include<bits/stdc++.h>
+#include<iostream>
+#include<queue>
+using namespace std;
+ 
+ 
+#define ll                      long long
+#define pint                    pair<int,int>
+#define plong                   pair<long long,long long>
+#define vlong                   vector <long long>
+#define vint                    vector <int>
+#define endl                    "\n"
+#define pb                      push_back
+#define mp                      make_pair
+#define for0(i,n)               for(int i=0;i<n;i++)
+#define for1(i,n)               for(int i=1;i<=n;i++)
+template <class t>              void printvector(vector<t>& v, ll n) {for0(i,n) {cout<<v[i]<<" ";} cout<<endl;}
+//-----------------------------------------------------------------------------------------------------------
+
+struct Node 
+{
+    int data;
+    Node* left;
+    Node* right;
+    
+    // Constructor
+    Node(int value) 
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* insertNode(Node* root, int value) 
+{
+    if (root == NULL) 
+    {
+        Node* head=new Node(value);
+        return head;
+    }
+    
+    if (value < root->data) 
+    {
+        root->left = insertNode(root->left, value);
+    }
+    else 
+    {
+        root->right = insertNode(root->right, value);
+    }
+    
+    return root;
+}
+
+void inorderTraversal(Node* root) 
+{
+    if (root == NULL) 
+        return;
+    
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+
+
+int main() 
+{
+    Node* root = NULL;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int k=0;
+        cin>>k;
+        root= insertNode(root,k);
+    }
+    
+    inorderTraversal(root);
+    cout<<endl;
+
+    Node* prev=NULL;
+    
+    return 0;
+}
